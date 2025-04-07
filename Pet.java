9b99297add9b5a06a3a8c5c53cc131be0bb5747f
definitelyjoseph <governormorgan2017@gmail.com>
2025-04-07 09:34:53 -0500

    Pet Class implementation

--- Pet.java
+++ Pet.java
@@ -1,3 +1,63 @@
 public class Pet {
-    
+    private String aniD;
+    private String name;
+    private String breed;
+    private int age;
+    private boolean adopted;
+
+    public Pet(String aniD, String name, String breed, int age) {
+        this.aniD = aniD;
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.adopted = false; // Default value for adopted
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getBreed(){
+        return breed;
+    }
+
+    public String getSpecies(){
+
+        switch (aniD.split("_")[0]) {
+            case "D":
+                return "Dog";
+            case "C":
+                return "Cat";
+            case "B":
+                return "Bird";
+            case "R":
+                return "Reptile";
+            case "F":
+                return "Fish";
+            default:
+                return "Unknown";
+        }
+        
+
+    }
+
+    public String getId(){
+        return aniD;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public boolean getAdoptionStat(){
+        return adopted;
+    }
+
+    public void setAdoptionStat(boolean adopted) {
+        this.adopted = adopted;
+    }
+
+    public String toString() {
+        return "Pet ID: " + aniD + ", Name: " + name + ", Breed: " + breed + ", Age: " + age + ", Adopted: " + adopted;
+    }
 }
